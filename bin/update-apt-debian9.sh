1137aad2c493f178dc0b8e19580f18b638f12043
ropzsu <zelzel.zsu@gmail.com>
2019-04-25 19:52:23 +0800

    Update update-apt-debian9.sh

--- bin/update-apt-debian9.sh
+++ bin/update-apt-debian9.sh
@@ -1,14 +1,18 @@
-sudo chmod 0755 /etc/apt/sources.list 
-cat > /etc/apt/sources.list <<EOF
+# Thinkpad T43 debian 9 
+
+$sudo vi /etc/apt/sources.list 
 deb http://mirrors.aliyun.com/debian/ stretch main contrib non-free
 deb http://mirrors.aliyun.com/debian-security stretch/updates main contrib non-free
-EOF
-sudo chmod 0644 /etc/apt/sources.list 
+ 
 
+# for golang
 sudo apt-get  update
 sudo apt-get -y install libc6-dev-i386 golang
 
+# for acme-editor
 sudo apt-get -y install gcc libx11-dev libxt-dev libxext-dev libfontconfig1-dev 
+
+# for nnn
 sudo apt-get -y install pkg-config libncursesw5-dev
 
 sudo apt-get -y install mpv 
